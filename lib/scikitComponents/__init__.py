46e156d7cdf4322300aed24fd0849758ddc5385a
Jake Hodges <jake.hodges@hotmail.co.uk>
2015-04-19 14:43:24 +0100

    Erroneous code to show Joe

--- lib/scikitComponents/__init__.py
+++ lib/scikitComponents/__init__.py
@@ -1,6 +1,8 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import gensim
 import numpy as np
+import itertools
+import collections
 
 class LDAModel(BaseEstimator, TransformerMixin):
 
@@ -41,3 +43,24 @@ class TopicMatrixBuilder(BaseEstimator, TransformerMixin):
 
 		return features[:,np.sum(features > 0, axis=0)>self.topic_min_members]
 
+
+class MetaMatrixBuilder(BaseEstimator, TransformerMixin):
+	def __init__(self, top_min_members = 0):
+		self.topic_min_members = 0
+
+	def fit(self, X, y=None):
+		return self
+
+	def transform(self, X, y=None):
+		""" Convert a TransformedCorpus object to a matrix of corpus vs topic index """
+		lists = [set(x.get('keyphrases', [])) | set(x.get('keywords', [])) for x in X]
+		sanitize = lambda word: word.strip()
+		counts = collections.Counter(sanitize(word) for word in itertools.chain(*lists))
+		words = [x for x in counts if counts[x] > 4 and counts[x] < len(X)-4 and x != '']
+
+		features = np.zeros((len(X), len(words)), dtype=int)
+		for i, doc in enumerate(lists):
+		    features[i,[words.index(x) for x in doc if x in words]] += 1
+
+		# return features[:,np.sum(features > 0, axis=0)>self.topic_min_members]
+		return features

b0b801f5ffe4fd55482f8237ee4651e36985af83
Joseph Atkins-Turkish <jat1g11@soton.ac.uk>
2015-04-20 10:27:53 +0100

    Code to proportionalise the dataset.

--- lib/datastuff.py
+++ lib/datastuff.py
@@ -3,10 +3,20 @@ import string
 from itertools import combinations_with_replacement
 import inspect, os
 from bs4 import BeautifulSoup
+import numpy as np
 
 with open(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))+"/stop_words.json") as f:
 	stoplist = set(json.load(f))
 
+def make_proportional(X, y, min_size = 100):
+    counts = np.bincount(y)
+    min_size = min(counts[counts>min_size])
+    class_inds = (np.where(y == i)[0] for i in range(max(y)+1))
+    indices = np.array(
+        [np.random.choice(inds, size=min_size, replace=False, p=None) for inds in class_inds 
+         if len(inds) >= min_size]
+    ).flatten()
+    return (X[indices], y[indices])
 
 def loadSplitJsonLines(fname):
 	''' Load every file matching the sequence fname.aa, fname.ab ... fname.zz

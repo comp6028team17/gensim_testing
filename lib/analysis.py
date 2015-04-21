de3d0d5345616f2b9ff310d90d570e6c6ac58650
Joseph Atkins-Turkish <jat1g11@soton.ac.uk>
2015-04-21 20:10:46 +0100

    Moved proportion/score plotter to analysis.py

--- lib/analysis.py
+++ lib/analysis.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import collections
 
 def plot_confusion_matrix(cm, title='Confusion matrix', labels=None, cmap=plt.cm.Blues):
     labels = labels if labels is not None else range(len(cm))
@@ -12,3 +13,29 @@ def plot_confusion_matrix(cm, title='Confusion matrix', labels=None, cmap=plt.cm
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
+
+
+def plot_proportion_investigation(predicted, dmoz_encoder, y_test, adjust_labels=False):
+    category_scores = collections.defaultdict(float)
+    category_counts = collections.defaultdict(float)
+
+    for real, pred in sorted(zip(dmoz_encoder.inverse_transform(y_test), dmoz_encoder.inverse_transform(predicted))):
+        category_scores[real] += 1 if real == pred else 0
+        category_counts[real] += 1 
+    data = np.array(sorted([
+                (category_counts[cat]/len(y_test), category_scores[cat] / category_counts[cat]) 
+                for cat in category_counts.keys()
+            ])).T
+    plt.plot(*data, marker='o')
+    plt.xlabel('proportions')
+    plt.ylabel('scores')
+    plt.title('Proportion of categories in test-set versus category score')
+    i = -1
+    for s, tx, ty in zip(category_counts.keys(), *data):
+        if adjust_labels:
+            tx = -0.0005+tx+i*0.001
+        else:
+            tx+=0.001
+        plt.text(tx, ty, s)
+        i*=-1
+    plt.show()

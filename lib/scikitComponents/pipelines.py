014680d3fb91e2204786534e08f1df9327337963
Joseph Atkins-Turkish <jat1g11@soton.ac.uk>
2015-04-29 23:06:13 +0100

    Mmmm, boxplots

--- lib/scikitComponents/pipelines.py
+++ lib/scikitComponents/pipelines.py
@@ -2,6 +2,7 @@ import sklearn
 import sklearn.neighbors
 from sklearn.pipeline import Pipeline, FeatureUnion
 import sklearn.grid_search
+import sklearn.naive_bayes
 
 from . import *
 def make_lda_pipeline(dictionary, num_topics):
@@ -24,7 +25,23 @@ def make_tfidf_matrix_pipeline(dictionary):
 
 
 
-def make_classifier(dictionary, classifier = 'svc', body_kind = 'tfidf', meta_kind='tfidf', meta_selection_flags = 13, lda_num = 30):
+def make_classifier(dictionary, classifier = 'svc', body_kind = 'tfidf', meta_kind='tfidf', meta_selection_flags = 13, lda_num = 20):
+    clfnames = {
+    'svc': 'SVM',
+    'svc_gridsearch': 'SVM',
+    'trees': 'DecisionTrees',
+    'nb': 'NaiveBayes'
+    }
+    bodynames = {
+    'tfidf': "TfIdf(body)",
+    'lda': 'LDA(body)'
+    }
+    metanames = {
+    'tfidf': "TfIdf(meta)",
+    'count': "Count(meta)"
+    }
+
+
     if classifier == 'svc_gridsearch': 
         param_grid = [{'C': [0.1, 0.16, 0.2, 0.3, 0.4, 0.5, 1]}]
         #param_grid = [{'C': [0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19]}]
@@ -36,6 +53,8 @@ def make_classifier(dictionary, classifier = 'svc', body_kind = 'tfidf', meta_ki
         clf = sklearn.neighbors.KNeighborsClassifier()
     elif classifier == 'trees':
         clf = sklearn.ensemble.ExtraTreesClassifier(random_state=0, n_estimators=100, oob_score=True, bootstrap=True, n_jobs=4)
+    elif  classifier == 'nb':
+        clf = sklearn.naive_bayes.MultinomialNB()
 
     body_pipe = meta_pipe = None
     
@@ -45,7 +64,6 @@ def make_classifier(dictionary, classifier = 'svc', body_kind = 'tfidf', meta_ki
         body_pipe = make_tfidf_matrix_pipeline(dictionary)
     elif body_kind == 'lda':
         body_pipe = make_lda_pipeline(dictionary, lda_num)
-
     if meta_kind == 'tfidf':
         meta_pipe = make_tfidf_matrix_pipeline(dictionary)
         meta_i = 1
@@ -60,10 +78,15 @@ def make_classifier(dictionary, classifier = 'svc', body_kind = 'tfidf', meta_ki
         features.append(('meta', Pipeline(
                 [('selector', ItemPicker(meta_i)), ('feature', meta_pipe)])))
     assert len(features) > 0
-        
-    return Pipeline([
+
+    pipe = Pipeline([
         ('union', FeatureUnion(features)), 
         ('clf', clf)])
+
+    pipe.name = " ".join(filter(lambda x: x, (bodynames.get(body_kind, None), metanames.get(meta_kind, None)))) + " " + clfnames[classifier]
+
+
+    return pipe
 
 
 
